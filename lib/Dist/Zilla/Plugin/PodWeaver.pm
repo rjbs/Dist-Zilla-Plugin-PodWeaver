63aa75229af0081fc936156aeb72b2a1b62258ca
Ricardo Signes <rjbs@cpan.org>
2009-10-27 22:35:44 -0400

    add config_plugin to let me write @RJBS !

--- lib/Dist/Zilla/Plugin/PodWeaver.pm
+++ lib/Dist/Zilla/Plugin/PodWeaver.pm
@@ -3,7 +3,7 @@ package Dist::Zilla::Plugin::PodWeaver;
 use Moose;
 use Moose::Autobox;
 use List::MoreUtils qw(any);
-use Pod::Weaver 3.093000; # @CorePrep as part of default config
+use Pod::Weaver 3.093001; # @Default
 with 'Dist::Zilla::Role::FileMunger';
 
 use namespace::autoclean;
@@ -13,6 +13,7 @@ use Pod::Elemental;
 use Pod::Elemental::Transformer::Pod5;
 use Pod::Elemental::Transformer::Nester;
 use Pod::Elemental::Selectors -all;
+use Pod::Weaver::Config::Assembler;
 
 =head1 WARNING
 
@@ -30,21 +31,40 @@ reconstructs it as boring old real POD.
 
 =head1 CONFIGURATION
 
-If a file matching C<./weaver.*> exists, Pod::Weaver will be told to look for
-configuration in the current directory.  Otherwise, it will use the default
-configuration.
+If the C<config_plugin> attribute is given, it will be treated like a
+Pod::Weaver section heading.  For example, C<@Default> could be given.
+
+Otherwise, if a file matching C<./weaver.*> exists, Pod::Weaver will be told to
+look for configuration in the current directory.
+
+Otherwise, it will use the default configuration.
 
 =cut
 
 sub _weaver {
   my ($self) = @_;
 
-  if (glob('weaver.*')) {
+  if ($self->config_plugin) {
+    my $assembler = Pod::Weaver::Config::Assembler->new;
+
+    my $root = $assembler->section_class->new({ name => '_' });
+    $assembler->sequence->add_section($root);
+
+    $assembler->change_section( $self->config_plugin );
+    $assembler->end_section;
+
+    return Pod::Weaver->new_from_config_sequence($assembler->sequence);
+  } elsif (glob('weaver.*')) {
     return Pod::Weaver->new_from_config;
   } else {
     return Pod::Weaver->new_with_default_config;
   }
 }
+
+has config_plugin => (
+  is  => 'ro',
+  isa => 'Str',
+);
 
 sub munge_file {
   my ($self, $file) = @_;
